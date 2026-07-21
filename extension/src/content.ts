e6dfbff38af1e4ba2ea8faaa4660297d15b313d9
Himanshu <ui.dev1@notenetic.com>
2026-07-21 23:54:53 +0530

    git commit -m "feat: implement popup to background to content script messaging"

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,1 +1,14 @@
-console.log("Content Script Loaded");
+console.log("🌐 Content Script Loaded");
+
+declare const chrome: any;
+
+chrome.runtime.onMessage.addListener((message: any) => {
+  console.log("📨 Content Script Received:", message);
+
+  if (message.type === "HIGHLIGHT_PAGE") {
+    document.body.style.backgroundColor = "yellow";
+    document.body.style.border = "8px solid green";
+
+    console.log("🎉 Page Highlighted");
+  }
+});
