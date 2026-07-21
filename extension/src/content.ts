a200a8ac73a3a5d9a838a30790ce8928b126a9f2
Himanshu <ui.dev1@notenetic.com>
2026-07-21 23:58:51 +0530

    done codeFormatting and Removed unused things

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -3,12 +3,12 @@ console.log("🌐 Content Script Loaded");
 declare const chrome: any;
 
 chrome.runtime.onMessage.addListener((message: any) => {
-  console.log("📨 Content Script Received:", message);
+    console.log("📨 Content Script Received:", message);
 
-  if (message.type === "HIGHLIGHT_PAGE") {
-    document.body.style.backgroundColor = "yellow";
-    document.body.style.border = "8px solid green";
+    if (message.type === "HIGHLIGHT_PAGE") {
+        document.body.style.backgroundColor = "yellow";
+        document.body.style.border = "8px solid green";
 
-    console.log("🎉 Page Highlighted");
-  }
+        console.log("🎉 Page Highlighted");
+    }
 });
